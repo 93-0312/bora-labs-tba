32dbc36d2cc62e318ab0e59ca8c88e0230461c10
chaeryn park <claire.r@metabora.io>
2023-10-11 15:54:35 +0900

    Update not found responsive ui

--- src/components/service/NotFound.vue
+++ src/components/service/NotFound.vue
@@ -1,9 +1,9 @@
 <template>
-  <main class="w-full max-w-[1200px] mx-auto">
-    <img :src="notFoundImg" alt="page not found" class="w-full max-w-lg h-auto mx-auto mt-7" />
-    <strong class="block mt-12 text-center text-3xl">Page not found</strong>
-    <p class="mt-3 text-center">Could you please check and try again?</p>
-    <router-link to="/" class="flex btn btn-accent btn-wide mt-12 mb-24 mx-auto">Home</router-link>
+  <main class="flex flex-col justify-center w-full max-w-[1200px] mx-auto">
+    <img :src="notFoundImg" alt="page not found" class="w-full max-w-xs h-auto mx-auto mt-3 md:max-w-lg md:mt-7" />
+    <strong class="block mt-6 text-center text-2xl md:text-3xl md:mt-12">Page not found</strong>
+    <p class="mt-2 text-center text-sm md:mt-3 md:text-md">Could you please check and try again?</p>
+    <router-link to="/" class="flex btn btn-accent btn-wide mt-8 mb-12 mx-auto md:mt-12 md:mb-24">Home</router-link>
   </main>
 </template>
 
